c27a79206ea0f46b31ecb8da37c01dc380c9d78c
Gualberto Ringström <gualberto@primea.se>
2022-05-31 15:39:24 +0200

    sorry for all commits

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     const [tramDataMunk, setTramDataMunk] = useState();
     const url = `https://api.weatherapi.com/v1/forecast.json?key=6e9629053cbb4418b10134951221105&q=${searchResult}&days=10&aqi=yes&alerts=no/`;
     useEffect(() => {
-       // setInterval( () => {
+       setInterval( () => {
             axios
                 .get(url)
                 .then((result) => {
@@ -27,10 +27,10 @@ function App() {
                     console.log(err);
                     toast.error('Error loading data, check the spellings');
                 });
-        //}, 60000 * 13)
+        }, 60000 * 13)
     }, []);
     useEffect(() => {
-        //setInterval(() => {
+        setInterval(() => {
             axios                                               //A=1@O=Göteborg Solrosgatan@X=12025239@Y=57716392@U=1@L=740025685@B=1@p=1653064537@
                 //'https://api.resrobot.se/v2.1/location.name?input=Munkebäckstorget, Göteborg?&format=json&accessId=525ebb4a-8664-4407-b822-cba358d8c7e7'
                 //.get('https://api.resrobot.se/v2.1/departureBoard?id=A=1@O=Göteborg Solrosgatan@X=12025239@Y=57716392@U=1@L=740025685@B=1@p=1653064537@"&format=json&duration=45&accessId=5b54df0d-4e16-4de1-8584-bdc99d5fad93)
@@ -42,10 +42,10 @@ function App() {
                     console.log(err);
                     toast.error('Error loading data, check the spellings');
                 });
-        //}, 60000 * 13);
+        }, 60000 * 13);
     }, []);
     useEffect(() => {
-        //setInterval(() => {
+        setInterval(() => {
             axios                                               //A=1@O=Göteborg Solrosgatan@X=12025239@Y=57716392@U=1@L=740025685@B=1@p=1653064537@
                 //'https://api.resrobot.se/v2.1/location.name?input=Munkebäckstorget, Göteborg?&format=json&accessId=525ebb4a-8664-4407-b822-cba358d8c7e7'
                 //.get('https://api.resrobot.se/v2.1/departureBoard?id=A=1@O=A=1@O=Göteborg Munkebäckstorget@X=12026407@Y=57718496@U=1@L=740025661@B=1@p=1653064537@"&format=json&duration=45&accessId=5b54df0d-4e16-4de1-8584-bdc99d5fad93)
@@ -57,7 +57,7 @@ function App() {
                     console.log(err);
                     toast.error('Error loading data, check the spellings');
                 });
-        //}, 60000 * 13);
+        }, 60000 * 13);
     }, []);
 
     return (
